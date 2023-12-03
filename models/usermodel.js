8259bfbf4136adc5f521de2d278811395808cb0b
yash <ymandlekar9826@gmail.com>
2023-12-03 16:14:05 +0530

    updating

--- models/usermodel.js
+++ models/usermodel.js
@@ -3,9 +3,12 @@ const plm = require("passport-local-mongoose");
 const userSchema = mongoose.Schema({
   email: String,
   fullname: String,
-  username: String,
+  username: {
+    type: String,
+    unique: true,
+  },
   password: String,
-  bio : String,
+  bio: String,
   dp: { type: String, default: "def.png" },
   role: { type: String, default: "user" },
   blocked: { type: Boolean, default: false },
@@ -15,7 +18,7 @@ const userSchema = mongoose.Schema({
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
   story: [{ type: mongoose.Schema.Types.ObjectId, ref: "story" }],
   is_online: { type: String, default: "0" },
-  key : String
+  key: String,
 });
 userSchema.plugin(plm);
 module.exports = mongoose.model("user", userSchema);
